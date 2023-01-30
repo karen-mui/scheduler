b2874054e3524880e11280426e3bfb081871fbc2
Karen Mui <KarenMui@Karens-MacBook-Pro.local>
2023-01-30 12:47:54 -0500

    fixed hardcoded data to localhost

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -15,9 +15,9 @@ export default function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
@@ -35,7 +35,7 @@ export default function useApplicationData() {
 
 
     return (
-      axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
+      axios.put(`/api/appointments/${id}`, appointment)
       .then(() => {
         const updatedDays = updateSpot(state.day, state.days, "REMOVE_SPOT", id, state.appointments)
         setState({...state, appointments, days: updatedDays})
@@ -54,7 +54,7 @@ export default function useApplicationData() {
     };
 
     return (
-      axios.delete(`http://localhost:8001/api/appointments/${id}`)
+      axios.delete(`/api/appointments/${id}`)
       .then(() => {
         const updatedDays = updateSpot(state.day, state.days, "ADD_SPOT", id, state.appointments)
         setState({...state, appointments, days: updatedDays})
